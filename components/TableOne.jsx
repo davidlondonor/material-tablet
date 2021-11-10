228fb539f84212fa21827fb5ba9f898f360b56a4
davidlucky@gmail.com <davidlucky@gmail.com>
2021-11-09 21:43:50 -0500

    advanced filtering

--- components/TableOne.jsx
+++ components/TableOne.jsx
@@ -40,30 +40,81 @@ const TableOne = () => {
       city: "Medellín",
       fee: 203498,
     },
+    {
+      name: "Judith",
+      email: "correo@correo.com",
+      phone: 3434343,
+      age: null,
+      gender: "F",
+      city: "Medellín",
+      fee: 203498,
+    },
+    {
+      name: "Natalia",
+      email: "correo@correo.com",
+      phone: 3434343,
+      age: 30,
+      gender: "F",
+      city: "Medellín",
+      fee: 203498,
+    },
+    {
+      name: "Judith",
+      email: "correo@correo.com",
+      phone: 3434343,
+      age: null,
+      gender: "F",
+      city: "Medellín",
+      fee: 203498,
+    },
+    {
+      name: "Natalia",
+      email: "correo@correo.com",
+      phone: 3434343,
+      age: 30,
+      gender: "F",
+      city: "Medellín",
+      fee: 203498,
+    },
   ]);
   const columns = [
-    { title: "Name", field: "name", sorting: false },
-    { title: "Email", field: "email" },
-    { title: "Phone Number", field: "phone", align: "right" },
+    { title: "Name", field: "name", sorting: false, filtering: false },
+    { title: "Email", field: "email", filterPlaceholder: "Filtrar el correo" },
+    {
+      title: "Phone Number",
+      field: "phone",
+      align: "right",
+      filterPlaceholder: "Filtrar numero",
+    },
     {
       title: "Age",
       field: "age",
       emptyValue: () => <em>null</em>,
       defaultSort: "asc",
+      filterPlaceholder: "Filtrar edad",
     },
-    { title: "Gender", field: "gender", lookup: { M: "male", F: "Female" } },
-    { title: "City", field: "city" },
+    {
+      title: "Gender",
+      field: "gender",
+      lookup: { M: "male", F: "Female" },
+      filterPlaceholder: "Filtrar mail",
+      exports: false,
+    },
+    { title: "City", field: "city", filterPlaceholder: "Filtrar Ciudad" },
     {
       title: "School Fee",
       field: "fee",
       type: "currency",
       currencySetting: { currencyCode: "COP", minimumFractionDigits: 1 },
+      filterPlaceholder: "Filtrar escuela",
     },
   ];
 
   {
     /* sorting es para evitar flechita en titulos */
     /* search es para evitar buscar */
+    // searchFieldAlignment: left/light alinear campo search
+    // searchAutoFocus: true o False
   }
   return (
     <>
@@ -78,7 +129,18 @@ const TableOne = () => {
         title="Título de la Tabla"
         columns={columns}
         data={tableData}
-        options={{ sorting: true }}
+        options={{
+          sorting: true,
+          filtering: true,
+          paging: true,
+          pageSizeOptions: [5, 20, 35, 55, 80],
+          pageSize: 5,
+          paginationType: "stepped",
+          showFirstLastPageButtons: false,
+          paginationPosition: "both",
+          exportButton: true,
+          exportAllData: true,
+        }}
       />
     </>
   );
